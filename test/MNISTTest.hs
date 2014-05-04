87f25c795a2e31ed10cee35ada6c21947e9df720
Johannes Drever <drever@lrz.uni-muenchen.de>
2014-05-04 04:24:34 +0200

    Changed absolute path to data to realtive path

--- test/MNISTTest.hs
+++ test/MNISTTest.hs
@@ -3,5 +3,5 @@ module Main where
 import ZuriRBM.TrainingData
 import ZuriRBM.MNIST
 
-main = do mnist <- readFromFilePath "/Users/halcyon/Documents/Coding/ZuriRBM/data" :: IO MNIST
+main = do mnist <- readFromFilePath "./data" :: IO MNIST
           putStrLn $ show mnist
