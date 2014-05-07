a51b3b9a710bc6d42a82732584b2c6733859e6d7
Johannes Drever <drever@lrz.uni-muenchen.de>
2014-05-07 02:05:14 +0200

    Started sketching the contrastive divergence algorithm.

---
+++ src/ZuriRBM/Learning.hs
@@ -0,0 +1,13 @@
+module ZuriRBM.Learning where
+
+import Numeric.LinearAlgebra
+
+
+contrastiveDivergence = undefined
+
+
+
+cdStep :: WeightMatrix -> StateVector -> StateVector -> ?
+
+coactivityMatrix :: (Matrix a) => StateVector -> StateVector -> a
+coactivityMatrix a b = outer and a b
